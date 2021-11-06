ff5539e308efc4ea75e8aab207c91e5aaeffb609
YMingF <834319313@qq.com>
2021-11-06 16:50:51 +0800

    用setOption完成数据更新

--- src/main.js
+++ src/main.js
@@ -1,20 +1,46 @@
 const echarts = require('echarts')
 let main = document.getElementById('main')
 let myChart = echarts.init(main)
+const loadMoreButton = document.getElementById('loadMore')
+let n = 0
+let m=0
+function createKey() {
+  n += 1
+  return `1-${n}`
+}
+
+function createValue() {
+  m += 10
+  return m
+}
+
+const xData = [createKey(), createKey(), createKey()] //存储横坐标信息
+const yValue = [createValue(), createValue(), createValue()] //存储纵坐标值
 // 使用刚指定的配置项和数据显示图表。
 myChart.setOption({
-  xAxis: {
-    type: 'category',
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  series: [
-    {
-      data: [150, 230, 224, 218, 135, 147, 260],
-      type: 'line'
-    }
-  ]
-}
+    xAxis: {
+      type: 'category',
+      data: xData
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: yValue,
+        type: 'line'
+      }
+    ]
+  }
 )
+loadMoreButton.addEventListener('click', () => {
+  //用setOption只改你要改的部分
+  myChart.setOption({
+    xAxis: {
+      data: [...xData, createKey()]
+    },
+    series: [
+      {data: [...yValue, createValue()]}
+    ]
+  })
+})
